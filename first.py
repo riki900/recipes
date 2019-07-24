0fbfa6f42a5242a8987f627c5499c41d4c7f939a
riki <kleinr901@gmail.com>
2019-07-24 13:33:42 +0200

    test with more allrecipes.com pages

--- first.py
+++ first.py
@@ -1,7 +1,16 @@
 from recipe_scrapers import *
 import json
 
-extracted = scrape_me('https://www.allrecipes.com/recipe/25317/carrot-chile-and-cilantro-soup/')
+def make_file_name(recipe) -> str:
+    prefix, _ = recipe['host'].split('.')
+    name = recipe['title'].replace(' ','_')
+    return f'{prefix}_{name}.json'
+
+target_url = 'https://www.allrecipes.com/recipe/25317/carrot-chile-and-cilantro-soup/'
+target_url = 'https://www.allrecipes.com/recipe/212722/murgh-makhani-indian-butter-chicken/'
+target_url = 'https://www.allrecipes.com/recipe/238522/moroccan-lentil-soup-with-veggies/?internalSource=rr_feed_recipe_sb&referringId=238523%20referringContentType%3Drecipe'
+
+extracted = scrape_me(target_url)
 
 recipe = { 'host': extracted.host(),
             'title': extracted.title(),
@@ -11,7 +20,9 @@ recipe = { 'host': extracted.host(),
             'instructions': extracted.instructions(),
             'ratings': extracted.ratings(),
             'reviews': extracted.reviews(),
+            'URL': target_url,
 }
 
-print('foo')
-print( json.dumps(recipe, sort_keys=True, indent=4))
+
+with open(make_file_name(recipe), 'w') as f:
+    f.write(json.dumps(recipe, sort_keys=True, indent=4))
