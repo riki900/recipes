7895802f6cf80d14db8465e2f3d3874cec922b5d
riki <kleinr901@gmail.com>
2019-08-11 13:09:45 +0200

    writing allrecipes url data to json

--- first.py
+++ first.py
@@ -1,28 +1,43 @@
 from recipe_scrapers import *
 import json
-
-def make_file_name(recipe) -> str:
-    prefix, _ = recipe['host'].split('.')
-    name = recipe['title'].replace(' ','_')
-    return f'{prefix}_{name}.json'
-
-target_url = 'https://www.allrecipes.com/recipe/25317/carrot-chile-and-cilantro-soup/'
-target_url = 'https://www.allrecipes.com/recipe/212722/murgh-makhani-indian-butter-chicken/'
-target_url = 'https://www.allrecipes.com/recipe/238522/moroccan-lentil-soup-with-veggies/?internalSource=rr_feed_recipe_sb&referringId=238523%20referringContentType%3Drecipe'
-
-extracted = scrape_me(target_url)
-
-recipe = { 'host': extracted.host(),
-            'title': extracted.title(),
-            'total_time': extracted.total_time(),
-            'yields': extracted.yields(),
-            'ingredients': extracted.ingredients(),
-            'instructions': extracted.instructions(),
-            'ratings': extracted.ratings(),
-            'reviews': extracted.reviews(),
-            'URL': target_url,
-}
+import uuid
 
 
-with open(make_file_name(recipe), 'w') as f:
-    f.write(json.dumps(recipe, sort_keys=True, indent=4))
+# def make_file_name(recipe) -> str:
+#     prefix, _ = recipe["host"].split(".")
+#     name = recipe["title"].replace(" ", "_").replace("(","").replace(")","").replace("'","").replace("&","")
+#     return f"json/{prefix}_{name}.json"
+
+
+def scrape_website(target_url):
+    extracted = scrape_me(target_url)
+
+    recipe = {
+        "host": extracted.host(),
+        "title": extracted.title(),
+        "total_time": extracted.total_time(),
+        "yields": extracted.yields(),
+        "ingredients": extracted.ingredients(),
+        "instructions": extracted.instructions(),
+        "ratings": extracted.ratings(),
+        "reviews": extracted.reviews(),
+        "URL": target_url,
+    }
+
+    file_name = str(uuid.uuid4())
+    with open(f"json/{file_name}.json", "w") as f:
+        f.write(json.dumps(recipe, sort_keys=True, indent=4))
+        f.write("\n")
+
+
+def read_urls(url_file):
+    with open("urls.trc", "w") as trc:
+        with open(url_file, "r") as urls:
+            for url in urls:
+                trc.write(f"{url}\n")
+                scrape_website(url)
+
+
+if __name__ == "__main__":
+    read_urls("urls.txt")
+    print("terminado")
--- read-todoist.py
+++ read-todoist.py
@@ -5,40 +5,40 @@
 import json
 import pprint
 import re
-from   collections import Counter
+from collections import Counter
 
 projects_file_path = "json/projects.json"
 tasks_file_path = "json/tasks.json"
 
-with open (projects_file_path, "r") as proj_export:
+with open(projects_file_path, "r") as proj_export:
     projs = json.load(proj_export)
 
-with open (tasks_file_path, "r") as task_export:
+with open(tasks_file_path, "r") as task_export:
     tasks = json.load(task_export)
 
 pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(tasks)
+# pp.pprint(tasks)
 
-#for proj in projs['projects']:
+# for proj in projs['projects']:
 #    print(f"{proj['id']} : {proj['name']}")
 
-recipe_projs = [2213378915,2214514337]   
+recipe_projs = [2213378915, 2214514337]
 
 print(type(tasks))
-print('scanning tasks')
+print("scanning tasks")
 # for task in tasks:
 #     if task['project_id'] in recipe_projs and \
 #        '//www.allrecipes.com' in task['content']:
 #             print(task['content'])
 
 tasks_list = {
-    task['content']
+    task["content"]
     for task in tasks
-    if task['project_id'] in recipe_projs and 'www.allrecipes.com' in task['content']
+    if task["project_id"] in recipe_projs and "www.allrecipes.com" in task["content"]
 }
 
-content_pattern = r'(?P<label>\[.+\])\((?P<url>.+)\)'
-host_pattern = r'\/\/(.+?)\/'
+content_pattern = r"(?P<label>\[.+\])\((?P<url>.+)\)"
+host_pattern = r"\/\/(.+?)\/"
 
 content_re = re.compile(content_pattern)
 host_re = re.compile(host_pattern)
@@ -53,12 +53,8 @@ for task_content in tasks_list:
     except IndexError as e:
         print(task_content)
 
-with open('urls.txt', 'w') as f:
+with open("urls.txt", "w") as f:
     for url in urls:
-        f.write(f'{url}\n')
-print('terminado')       
-
-
-
-
+        f.write(f"{url}\n")
+print("terminado")
 
